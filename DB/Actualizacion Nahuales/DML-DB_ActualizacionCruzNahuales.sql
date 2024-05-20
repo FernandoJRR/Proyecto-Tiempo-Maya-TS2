c63a85dd09d705ae2ebcb60815d24b55cd2e608d
FernandoJRR <fercitotron@gmail.com>
2024-05-20 14:24:16 -0600

    Add cruz maya to nahuales

---
+++ DB/Actualizacion Nahuales/DML-DB_ActualizacionCruzNahuales.sql
@@ -0,0 +1,40 @@
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_tzikin.png"/>') WHERE nahual.idweb = 0;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_ajmaq.png"/>') WHERE nahual.idweb = 1;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_noj.png"/>') WHERE nahual.idweb = 2;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_tijax.png"/>') WHERE nahual.idweb = 3;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_kawoq.png"/>') WHERE nahual.idweb = 4;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_ajpu.png"/>') WHERE nahual.idweb = 5;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_imox.png"/>') WHERE nahual.idweb = 6;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_iq.png"/>') WHERE nahual.idweb = 7;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_akabal.png"/>') WHERE nahual.idweb = 8;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_kat.png"/>') WHERE nahual.idweb = 9;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_kan.png"/>') WHERE nahual.idweb = 10;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_kame.png"/>') WHERE nahual.idweb = 11;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_kej.png"/>') WHERE nahual.idweb = 12;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_qanil.png"/>') WHERE nahual.idweb = 13;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_toj.png"/>') WHERE nahual.idweb = 14;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_tzi.png"/>') WHERE nahual.idweb = 15;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_batz.png"/>') WHERE nahual.idweb = 16;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_e.png"/>') WHERE nahual.idweb = 17;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_aj.png"/>') WHERE nahual.idweb = 18;
+UPDATE nahual SET nahual.htmlCodigo = CONCAT(nahual.htmlCodigo, '<p><strong>Cruz Maya:</strong></p>
+<img style="width:30%" src="../imgs/CruzMaya/cruz_ix.png"/>') WHERE nahual.idweb = 19;
